0bddcadbf78e30ec498e6add88926e8378911cbf
Felipe Mansilla <pipe.ny@gmail.com>
2016-04-05 13:37:54 -0400

    added mr brown's solution

--- hw06/password.py
+++ hw06/password.py
@@ -54,8 +54,34 @@ def strength_pass(password):
         print "Error: password does not meet requirements\n"
         check_pass(password)
 
-p = "myNoobPass1234"
+#Mr Brown's code
 
-check_pass(p)
+def strength_check(p):
+    l = [1 if x in UC_LETTERS else
+         2 if x in LC_LETTERS else
+         3 if x in NUMS else
+         0 for x in p]
+    return 1 in l and 2 in l and 3 in l
 
-strength_pass(p)
+def strength_rate(p):
+    l = [1 if x in UC_LETTERS else
+         2 if x in LC_LETTERS else
+         3 if x in NUMS else
+         4 if x in SPECIAL_CHAR else
+         0 for x in p]
+
+    uc = len(p) - l.count(1)
+    lc = len(p) - l.count(2)
+    nums = len(p) - l.count(3)
+    chars = len(p) - l.count(4)
+
+    strength = (uc + lc + nums + chars)
+    return strength
+
+p = "mynoobpassaqwe"
+
+print str(strength_rate(p))
+
+#check_pass(p)
+
+#strength_pass(p)
